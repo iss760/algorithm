9bfc559b86561c9ba8ca1fcf8a1cacc493eb49db
iss760 <gane774@naver.com>
2020-09-03 16:15:05 +0900

    Modify 005.Longest_Palindromic_Substring.py for speed

--- 005.Longest_Palindromic_Substring.py
+++ 005.Longest_Palindromic_Substring.py
@@ -14,20 +14,70 @@ class Solution:
         :param s: str
         :return: str
         """
-        size = len(s)
-        if size == 0:
-            return s
-        result = s[0]
-        max_len = 0
-        for i in range(size - 1):
-            for j in range(i + 1, size):
-                if self.isPalindrome(s[i:j + 1]) and max_len < len(s[i:j + 1]):
-                    max_len = len(s[i:j + 1])
-                    result = s[i:j + 1]
-        return result
+        if len(s) == 1:
+            return s[0]
 
-    def isPalindrome(self, s):
-        for i in range(len(s) // 2):
-            if s[i] != s[-1 - i]:
-                return False
-        return True
+        res = ""
+        for i in range(len(s)):
+            temp = s[i]
+            l, r = -1, 1
+
+            while l+i >= 0 and r+i <= len(s)-1:
+                if s[i+l] == s[i+r]:
+                    temp = s[i+l] + temp + s[i+r]
+                    l -= 1
+                    r += 1
+                else:
+                    break
+
+            res = temp if len(res) < len(temp) else res
+
+        for i in range(len(s) - 1):
+            if s[i] != s[i+1]:
+                continue
+            temp = s[i:i+2]
+            l, r = -1, 1
+
+            while l+i >= 0 and i+1+r <= len(s)-1:
+                if s[i+l] == s[i+1+r]:
+                    temp = s[i+l] + temp + s[i+1+r]
+                    l -= 1
+                    r += 1
+                else:
+                    break
+
+            res = temp if len(res) < len(temp) else res
+
+        return res
+
+    #     size = len(s)
+    #     if size == 0:
+    #         return s
+    #     result = s[0]
+    #     max_len = 0
+    #     for i in range(size - 1):
+    #         for j in range(i + 1, size):
+    #             if self.isPalindrome(s[i:j + 1]) and max_len < len(s[i:j + 1]):
+    #                 max_len = len(s[i:j + 1])
+    #                 result = s[i:j + 1]
+    #     return result
+    #
+    # def isPalindrome(self, s):
+    #     for i in range(len(s) // 2):
+    #         if s[i] != s[-1 - i]:
+    #             return False
+    #     return True
+
+
+sol = Solution()
+print(sol.longestPalindrome("babad"))
+print(sol.longestPalindrome('kxbabxzozy'))
+print(sol.longestPalindrome('kxyu'))
+print(sol.longestPalindrome(''))
+print(sol.longestPalindrome('cbbd'))
+print(sol.longestPalindrome('d'))
+print(sol.longestPalindrome('bb'))
+print(sol.longestPalindrome("ccc"))
+print(sol.longestPalindrome("aaaa"))
+print(sol.longestPalindrome("aaabaaaa"))
+
