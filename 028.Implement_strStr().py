9d384eabda370802992ea5e085195325174b3478
seohyeonpark <gane774@naver.com>
2020-09-13 22:15:34 +0900

    Add 028.Implement_strStr().py

---
+++ 028.Implement_strStr().py
@@ -0,0 +1,22 @@
+# Implement strStr().
+# Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+# Example 1:
+#   Input: haystack = "hello", needle = "ll"
+#   Output: 2
+# Example 2:
+#   Input: haystack = "aaaaa", needle = "bba"
+#   Output: -1
+
+
+class Solution:
+    def strStr(self, haystack, needle):
+        """
+        :param haystack: str
+        :param needle: str
+        :return: int
+        """
+        if needle not in haystack:
+            return -1
+
+        return haystack.index(needle)
