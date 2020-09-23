622a8ef3ddb6fd809449a298ac3f42031aceb574
iss760 <gane774@naver.com>
2020-09-23 17:07:54 +0900

    Add 034.Find_First_and_Last_Position_of_Element_in_Sorted_Array.py

---
+++ 034.Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -0,0 +1,30 @@
+# Given an array of integers nums sorted in ascending order,
+# find the starting and ending position of a given target value.
+# Your algorithm's runtime complexity must be in the order of O(log n).
+# If the target is not found in the array, return [-1, -1].
+
+# Example 1:
+#   Input: nums = [5,7,7,8,8,10], target = 8
+#   Output: [3,4]
+# Example 2:
+#   Input: nums = [5,7,7,8,8,10], target = 6
+#   Output: [-1,-1]
+
+
+class Solution:
+    def searchRange(self, nums: list, target: int) -> list:
+        if target in nums:
+            s = nums.index(target)
+            e = s + 1
+            while e < len(nums) and nums[s] == nums[e]:
+                e += 1
+            return [s, e-1]
+        else:
+            return [-1, -1]
+
+
+sol = Solution()
+print(sol.searchRange([5, 7, 7, 8, 8, 10], 8))
+print(sol.searchRange([5, 7, 7, 8, 8, 10], 6))
+print(sol.searchRange([5, 7, 7, 8, 8, 10, 11], 10))
+print(sol.searchRange([1], 1))
